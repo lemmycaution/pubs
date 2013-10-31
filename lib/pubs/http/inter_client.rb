fa3fd4f9efd3e3829c93ef5bf3008417a9aa670e
Onur Uyar <me@onuruyar.com>
2013-10-31 13:08:26 +0000

    inter client fix

--- lib/pubs/http/inter_client.rb
+++ lib/pubs/http/inter_client.rb
@@ -2,24 +2,24 @@ require 'pubs/http/client'
 
 module Pubs
   module HTTP
-    
-    class InternalClient < Client
-  
+
+    class InterClient < Client
+
       def initialize(cookie = "", api_key = "")
         @head = {
-          'user-agent' => self.class.name, 
+          'user-agent' => self.class.name,
           'cookie' => cookie,
           'X-Api-Key' => api_key
         }
       end
-    
+
       def request method, url, params = {}
         params.update({head: @head})
         http = EM::HttpRequest.new(url).send method, params
         http.response
       end
-    
+
     end
-  
+
   end
 end
